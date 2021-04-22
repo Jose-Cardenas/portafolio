b02cb2b5d1090c749af1bc3091e5f3d987c1b6c7
Jose <josefe.cardenas@gmail.com>
2021-04-22 01:29:06 -0500

    se agregaron los primeros proyectos, falta mucho estilo

---
+++ docs/scripts/svg.js
@@ -0,0 +1,70 @@
+function animation() {
+    circles();
+
+    setTimeout(() => {
+        paths()
+    }, 2000);
+    setTimeout(() => {
+        centercircle()
+    }, 4000)
+}
+
+animation()
+
+
+
+function circles() {
+    var circle = document.querySelectorAll("circle");
+    circle.forEach(element => {
+        if (element.id != "path852") {
+            var length = element.getTotalLength();
+            //set CSS properties up
+            element.style.strokeDasharray = length + ' ' + length;
+            element.style.strokeDashoffset = length;
+            element.getBoundingClientRect();
+            //set transition up
+            element.style.transition = 'stroke-dashoffset 2s ease-out';
+            // animate
+            element.style.strokeDashoffset = '0';
+        }
+    })
+}
+function centercircle() {
+    var circle = document.querySelector("#path852");
+    var length = circle.getTotalLength();
+    //set CSS properties up
+    circle.style.stroke = '#00adb9';
+    circle.style.opacity = "0"
+    circle.getBoundingClientRect();
+    //set transition up
+    circle.style.transition = 'opacity 1s linear';
+    // animate
+    circle.style.opacity = '1';
+}
+
+
+
+
+function paths() {
+    var path = document.querySelectorAll("path");
+    path.forEach(element => {
+        var length = element.getTotalLength();
+        //set CSS properties up
+        element.style.strokeDasharray = length + ' ' + length;
+        element.style.strokeDashoffset = length;
+        element.style.stroke = "#00adb9"
+        element.getBoundingClientRect();
+        //set transition up
+        element.style.transition = 'stroke-dashoffset 2s ease-out';
+        // animate
+        element.style.strokeDashoffset = '0';
+
+    })
+}
+function gradientes() {
+    var gradiente = document.querySelectorAll("radialGradient")
+    gradiente.forEach(element => {
+    })
+}
+
+gradientes()
